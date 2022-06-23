96bfc7ddee690f699345a6333768b38acbca6d11
Graeme <graeme_c_smith@hotmail.com>
2022-06-23 15:08:49 +0100

    Added doc string to function

--- pytest_tutorial/pytest_tutorial.py
+++ pytest_tutorial/pytest_tutorial.py
@@ -1,21 +1,26 @@
-def is_miscall(male_partner, female_partner, embryo):
+def is_miscall(father, mother, child):
     """
     QC to identify miscalls
+
+    This function takes three strings as args representing a haplotype for a trio of samples (father, mother, and child).
+    The haplotypes can be "AA", "BB", or "AB".
+    The function checks the parents haplotype and if the childs haplotype does not correspond reports it as a Miscall.
+    For example, parents with aHaplotpe "AA" and "AA" cannot have a child with haplotype "BB".  This would suggest a technical issue with this result.
     """
     is_miscall = False
 
-    if male_partner == "AA" and female_partner == "AA" and embryo != "AA":
+    if father == "AA" and mother == "AA" and child != "AA":
         is_miscall = True
-    elif male_partner == "BB" and female_partner == "BB" and embryo != "BB":
+    elif father == "BB" and mother == "BB" and child != "BB":
         is_miscall = True
-    elif male_partner == "AA" and female_partner == "BB" and embryo != "AB":
+    elif father == "AA" and mother == "BB" and child != "AB":
         is_miscall = True
-    elif male_partner == "BB" and female_partner == "AB" and embryo != "AB":
+    elif father == "BB" and mother == "AB" and child != "AB":
         is_miscall = True
     elif (
-        male_partner == "AA"
-        and female_partner == "AB"
-        and embryo
+        father == "AA"
+        and mother == "AB"
+        and child
         not in [
             "AA",
             "AB",
@@ -23,9 +28,9 @@ def is_miscall(male_partner, female_partner, embryo):
     ):
         is_miscall = True
     elif (
-        male_partner == "BB"
-        and female_partner == "AB"
-        and embryo
+        father == "BB"
+        and mother == "AB"
+        and child
         not in [
             "BB",
             "AB",
@@ -33,9 +38,9 @@ def is_miscall(male_partner, female_partner, embryo):
     ):
         is_miscall = True
     elif (
-        male_partner == "AB"
-        and female_partner == "AA"
-        and embryo
+        father == "AB"
+        and mother == "AA"
+        and child
         not in [
             "AA",
             "AB",
@@ -43,9 +48,9 @@ def is_miscall(male_partner, female_partner, embryo):
     ):
         is_miscall = True
     elif (
-        male_partner == "AB"
-        and female_partner == "BB"
-        and embryo
+        father == "AB"
+        and mother == "BB"
+        and child
         not in [
             "BB",
             "AB",
@@ -55,8 +60,4 @@ def is_miscall(male_partner, female_partner, embryo):
     return is_miscall
 
 
-def check_df_for_miscalls(df):
-    pass
-
-
-is_miscall("AA", "BB", "BB")
+print(is_miscall("AA", "BB", "BB"))
