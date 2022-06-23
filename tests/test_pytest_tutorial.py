f951b00aef1c48c2e23dbf2cc213f93de4717116
Graeme <graeme_c_smith@hotmail.com>
2022-06-23 15:47:27 +0100

    Tweaking test

--- tests/test_pytest_tutorial.py
+++ tests/test_pytest_tutorial.py
@@ -1,5 +1,10 @@
 from pytest_tutorial import __version__
+from pytest_tutorial import *
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+def test_is_miscall_AA_AA_AB():
+    assert is_miscall("AA", "AA", "BB") == True
