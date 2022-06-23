b62a8c80b6cf3925ecea8d9ea5b585a0e193ee7a
Graeme <graeme_c_smith@hotmail.com>
2022-06-23 15:02:48 +0100

    Added images & function to test

--- pytest_tutorial/pytest_tutorial.py
+++ pytest_tutorial/pytest_tutorial.py
@@ -1,2 +1,62 @@
-for i in range(5):  # this comment has too many spaces
-    print(i)  # this line has 6 space indentation.
+def is_miscall(male_partner, female_partner, embryo):
+    """
+    QC to identify miscalls
+    """
+    is_miscall = False
+
+    if male_partner == "AA" and female_partner == "AA" and embryo != "AA":
+        is_miscall = True
+    elif male_partner == "BB" and female_partner == "BB" and embryo != "BB":
+        is_miscall = True
+    elif male_partner == "AA" and female_partner == "BB" and embryo != "AB":
+        is_miscall = True
+    elif male_partner == "BB" and female_partner == "AB" and embryo != "AB":
+        is_miscall = True
+    elif (
+        male_partner == "AA"
+        and female_partner == "AB"
+        and embryo
+        not in [
+            "AA",
+            "AB",
+        ]
+    ):
+        is_miscall = True
+    elif (
+        male_partner == "BB"
+        and female_partner == "AB"
+        and embryo
+        not in [
+            "BB",
+            "AB",
+        ]
+    ):
+        is_miscall = True
+    elif (
+        male_partner == "AB"
+        and female_partner == "AA"
+        and embryo
+        not in [
+            "AA",
+            "AB",
+        ]
+    ):
+        is_miscall = True
+    elif (
+        male_partner == "AB"
+        and female_partner == "BB"
+        and embryo
+        not in [
+            "BB",
+            "AB",
+        ]
+    ):
+        is_miscall = True
+    return is_miscall
+
+
+def check_df_for_miscalls(df):
+    pass
+
+
+is_miscall("AA", "BB", "BB")
